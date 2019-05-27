760e196167461b78bb24edd785e235a03e03082b
chinmay.padhye@verizonmedia.com <cpadhye@UNKNOWN-172-154-98-X.yahoo.com>
2019-05-27 16:47:26 +0530

    Added a test to test big files

--- enc-tree_test.go
+++ enc-tree_test.go
@@ -99,7 +99,7 @@ func testReadingFromDifferentFile(t *testing.T) {
 	}
 	EncodeIntoFile(f2, tree2)
 	f2.Close()
-	f2, err = os.Create("/tmp/testing-2")
+	f2, err = os.Open("/tmp/testing-2")
 	if err != nil {
 		fmt.Println("Error creating file")
 	}
@@ -115,4 +115,35 @@ func testReadingFromDifferentFile(t *testing.T) {
 		t.Errorf("Expected a comparison pass")
 	}
 
+}
+
+func TestVeryDeepTrees(t *testing.T) {
+	entries := make([]int, 0)
+	for idx := 0; idx < 10000; idx++ {
+		entries = append(entries, idx)
+	}
+
+	data := make([]Data, 0)
+	for idx := 0; idx < len(entries); idx++ {
+		s := strconv.Itoa(entries[idx])
+		data = append(data, Data{entries[idx], s})
+	}
+	tree := MakeTree(data)
+	f, err := os.Create("/tmp/testing-big-file-1")
+	if err != nil {
+		fmt.Println("Error creating file")
+	}
+	EncodeIntoFile(f, tree)
+	f.Close()
+	f1, err1 := os.Open("/tmp/testing-big-file-1")
+	if err1 != nil {
+		fmt.Println("Error creating file")
+	}
+	data2 := DecodeFile(f1)
+	tree2 := MakeTree(data2)
+	f1.Close()
+	cmp := CompareTrees(tree, tree2)
+	if cmp != true {
+		t.Errorf("Even big trees should be correctly encoded")
+	}
 }
